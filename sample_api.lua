0a5361b1e1514d055e0c8071e08de6136eecb35a
Orga Shih <orgashih@exosite.com>
2017-09-07 18:25:24 +0800

    update init

--- sample_api.lua
+++ sample_api.lua
@@ -415,18 +415,7 @@ response.message = debug(websocket_info.message)
 response.message = listen(websocketInfo)
 --#ENDPOINT GET /_init
 print("hello world init")
-local ret1 = User.createRole({role_id = "owner", parameter = {{name = "sn"}}})
-local ret2 = User.createRole({role_id = "guest", parameter = {{name = "sn"}}})
-local ret = ret1.status_code ~= nil and ret1 or nil
-if ret == nil then
-  ret = ret2.status_code ~= nil and ret2 or nil
-end
-if ret ~= nil then
-  response.code = ret.status_code
-  response.message = ret.message
-else
-  response.code = 200
-end
+
 --#ENDPOINT GET /tsdb/query/{sn}/{alias}
 local metrics = {
   request.parameters.alias
